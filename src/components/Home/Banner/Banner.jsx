bfbfef76445703f2d3a53d430c2927d962c611ea
IsmatJahanBijori <ismatjahanbijori@gmail.com>
2023-06-28 02:36:59 +0600

    banner edited

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -9,12 +9,12 @@ const Banner = () => {
                     <div className='hero-overlay rounded-xl bg-opacity-30 w-[500px] md:w-[1600px] h-[1000px] lg:h-[700px]'>
                         <div className='mt-16 md:mt-32 text-left flex flex-col lg:flex-row md:ml-20'>
                             <div className='ml-5'>
-                                <h1 className="mb-5 text-5xl font-bold">Hello <br /> Myself <span className='text-[#64CCC5]'>Bijori</span></h1>
-                                <p className="mb-5 text-3xl font-semibold">A Junior <span className='text-[#64CCC5]'>React Developer</span></p>
+                                <h1 className="mb-5 text-5xl font-bold md:text-center">Hello <br/> Myself <span className='text-[#64CCC5]'>Bijori</span></h1>
+                                <p className="mb-5 text-3xl md:text-center font-semibold">A Junior <br />  <span className='text-[#64CCC5]'>React Developer</span></p>
                                 <p className="mb-5 text-xl font-medium w-[300px] text-justify">Pave a prosperous journey in web development by honing my expertise in both front-end development. Aspire to craft user-friendly websites and applications that boast seamless functionality with visually appealing design, ultimately providing unparalleled user delight and fueling significant business results for clients.</p>
                             </div>
                             <div className='my-16 md:my-32 md:text-right ml-5 md:mx-20'>
-                                <img src="https://i.ibb.co/7WjWp0s/IMG-20210213-140721.jpg" className='w-[400px]' alt="" />
+                                <img src="https://i.ibb.co/7WjWp0s/IMG-20210213-140721.jpg" className='w-[250px] sm:w-[400px]' alt="" />
                             </div>
                         </div>
                     </div>
